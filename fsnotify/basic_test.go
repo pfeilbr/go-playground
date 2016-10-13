50de923917d82ae61dbada22b84e8ce8b2c34d06
Brian <brian.pfeil@gmail.com>
2016-10-13 14:13:14 -0400

    added sandbox to fsnotify for temp dir

--- fsnotify/basic_test.go
+++ fsnotify/basic_test.go
@@ -10,72 +10,69 @@ import (
 	fsnotify "gopkg.in/fsnotify.v1"
 )
 
-func TestWatchWrite(t *testing.T) {
-
+// sandbox is a helper to provide a temporary directory
+func sandbox(fn func(string)) {
 	tmpDir := "./tmp/"
-
 	os.RemoveAll(tmpDir)
 	os.MkdirAll(tmpDir, os.ModePerm)
 	defer os.RemoveAll(tmpDir)
+	fn(tmpDir)
+}
 
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestWatchWrite(t *testing.T) {
 
-	if watcher == nil {
-		t.Fatal("failed to create watcher.  watcher is nil")
-	}
-	defer watcher.Close()
+	sandbox(func(tmpDir string) {
+		var err error
+		var watcher *fsnotify.Watcher
 
-	result := make(chan interface{})
-	timeout := time.Millisecond * 200
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
+		if watcher, err = fsnotify.NewWatcher(); err != nil {
+			t.Fatal(fmt.Errorf("failed to create watcher: %s", err))
+		}
+		defer watcher.Close()
 
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					result <- fsnotify.Write
-				} else {
+		result := make(chan interface{})
+		timeout := time.Millisecond * 200
+		go func() {
+			for {
+				select {
+				case event := <-watcher.Events:
 					result <- event.Op
+				case err := <-watcher.Errors:
+					result <- err
+				case <-time.After(timeout):
+					result <- fmt.Errorf("event did not occur within timeout of %v", timeout)
 				}
-			case err := <-watcher.Errors:
-				result <- err
-			case <-time.After(timeout):
-				result <- fmt.Errorf("event did not occur within timeout of %v", timeout)
 			}
+		}()
+
+		// create file before adding to watcher
+		var f *os.File
+		if f, err = os.Create(filepath.Join(tmpDir, "a.txt")); err != nil {
+			t.Fatal(err)
 		}
-	}()
+		defer f.Close()
 
-	// create file before adding to watcher
-	f, err := os.Create(filepath.Join(tmpDir, "a.txt"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	err = watcher.Add(tmpDir)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// perform write
-	f.Write([]byte("hello"))
-
-	// wait on result
-	res := <-result
-
-	expect := fsnotify.Write
-	switch res.(type) {
-	case fsnotify.Op:
-		if res != fsnotify.Write {
-			t.Errorf("expect: %v, result: %v", expect, res)
+		if err = watcher.Add(tmpDir); err != nil {
+			t.Fatal(err)
 		}
-	case error:
-		t.Error(res)
-	default:
-		t.Fatal("unexpected response")
-	}
+
+		// perform write
+		f.Write([]byte("hello"))
+
+		// wait on result
+		res := <-result
+
+		expect := fsnotify.Write
+		switch res.(type) {
+		case fsnotify.Op:
+			if res != fsnotify.Write {
+				t.Errorf("expect: %v, result: %v", expect, res)
+			}
+		case error:
+			t.Error(res)
+		default:
+			t.Fatal("unexpected response")
+		}
+	})
 
 }
